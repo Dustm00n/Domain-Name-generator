689ba74c96919f4cc32e397c71d0b76dc32b0607
Dustm00n <gerardoserranosoto@gmail.com>
2022-01-21 18:17:54 +0000

    stompeado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,24 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { WatchIgnorePlugin } from "webpack";
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+};
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dominio = [".com", ".net", ".us", ".io"];
+
+
+
+let pronounindx = Math.floor(Math.random() * pronoun.length);
+let adjindx = Math.floor(Math.random() * adj.length);
+let nounindx = Math.floor(Math.random() * noun.length);
+let dominioindx = Math.floor(Math.random() * dominio.length);
+
+return pronoun[pronounindx] +" "+ adj[adjindx] +" "+ noun[nounindx] +" "+dominio[dominioindx];
 };
